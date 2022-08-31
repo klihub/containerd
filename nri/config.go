f0b0ad234416a61533ea32fb6c97b9c1de4968f5
Krisztian Litkey <krisztian.litkey@intel.com>
2022-08-31 16:10:24 +0300

    nri: add experimental NRI 'service' plugin.
    Add a common NRI 'service' plugin. It takes care of relaying
    requests and respones to and from NRI (external NRI plugins)
    and the high-level containerd namespace-independent logic of
    applying NRI container adjustments and updates to actual CRI
    and other containers.
    
    The namespace-dependent details of the necessary container
    manipulation operations are to be implemented by namespace-
    specific adaptations. This NRI plugin defines the API which
    such adaptations need to implement.
    
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

---
+++ nri/config.go
@@ -0,0 +1,54 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package nri
+
+import (
+	nri "github.com/containerd/nri/v2alpha1/pkg/adaptation"
+)
+
+// Config data for NRI.
+type Config struct {
+	Disable    bool   `toml:"disable" json:"disable"`
+	ConfigPath string `toml:"config_file" json:"configFile"`
+	SocketPath string `toml:"socket_path" json:"socketPath"`
+	PluginPath string `toml:"plugin_path" json:"pluginPath"`
+}
+
+// DefaultConfig returns the default configuration.
+func DefaultConfig() *Config {
+	return &Config{
+		Disable:    true,
+		ConfigPath: nri.DefaultConfigPath,
+		SocketPath: nri.DefaultSocketPath,
+		PluginPath: nri.DefaultPluginPath,
+	}
+}
+
+// toOptions returns NRI options for this configuration.
+func (c *Config) toOptions() []nri.Option {
+	opts := []nri.Option{}
+	if c.ConfigPath != "" {
+		opts = append(opts, nri.WithConfigPath(c.ConfigPath))
+	}
+	if c.SocketPath != "" {
+		opts = append(opts, nri.WithSocketPath(c.SocketPath))
+	}
+	if c.PluginPath != "" {
+		opts = append(opts, nri.WithPluginPath(c.PluginPath))
+	}
+	return opts
+}
